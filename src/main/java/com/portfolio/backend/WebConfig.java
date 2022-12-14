b2fff115d49d1aacf334963f865b7b66c7d761cf
marianopuchetta <marianopuchetta@hotmail.com>
2022-12-14 11:57:58 -0300

    added allowedOrigins

--- src/main/java/com/portfolio/backend/WebConfig.java
+++ src/main/java/com/portfolio/backend/WebConfig.java
@@ -15,6 +15,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
+        registry.addMapping("/**")
+        .allowedMethods("*")
+        .allowedOrigins("https://marianopuchetta-protfolio.web.app/");
     }
 }

bbe1b699ddb08f4792e0f97de0f8fe99ee4ebbf8
marianopuchetta <marianopuchetta@hotmail.com>
2025-07-15 20:00:55 -0300

    tipoCanasta CPA erased

--- src/main/java/com/basic_food_basket/model/TipoCanasta.java
+++ src/main/java/com/basic_food_basket/model/TipoCanasta.java
@@ -2,5 +2,5 @@ package com.basic_food_basket.model;
 
 public enum TipoCanasta {
     CBA, // Canasta Básica Alimentaria
-    CPA  // Canasta Personal Adulto
+   // CPA  // Canasta Personal Adulto
 }
